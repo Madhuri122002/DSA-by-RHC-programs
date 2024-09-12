f8028e9f550ffbd7145ae984e86ca0527c5fc22e
Madhuri <madhuriv1037@gmail.com>
2024-09-12 14:33:48 +0530

    commit

---
+++ Linked List/SinglyLinkedList.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data)
+    {
+        this ->data=data;
+        this ->next=NULL;
+    }
+    ~Node()
+    {
+        int value=this->data;
+        if(this->next!=NULL)
+        {
+            delete next;
+            this->next=NULL;
+        }
+    }
+};
+
+void insertAtTail(Node* &tail,int data)
+{
+    Node* temp=new Node(data);
+    tail->next=temp;
+    tail=temp;
+}
+void insertAtHead(Node* &head,int data)
+{
+    Node* temp=new Node(data);
+    temp->next=head;
+    head=temp;
+}
+void print(Node* &head)
+{
+    Node *temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next; 
+    }
+
+}
+
+void insertAtPosition(Node* &head,Node* &tail, int position, int data)
+{
+    Node* temp=head;
+    if(position==1)
+    {
+        insertAtHead(head, data);
+        return;
+    }
+    int count=1;
+    while(count<position-1)
+    {
+        temp=temp->next;
+    }
+
+    if(temp->next==NULL)
+    {
+        insertAtTail(tail,data);
+        return;
+    }
+    Node* newNode=new Node(data);
+
+
+    newNode->next=temp->next;
+    temp->next=newNode;
+}
+void deleteNode(Node* &head,int position)
+{
+    if(position==1)
+    {
+        Node* temp=head;
+        head=head->next;
+        temp->next=NULL;
+        delete temp;
+    }
+    else{
+        Node* curr=head;
+        Node* prev=NULL;
+        int count=1;
+        while(count<position)
+        {
+            prev=curr;
+            curr=curr->next;
+            count++;
+        }
+        prev->next=curr->next;
+        curr->next=NULL;
+        delete curr;
+    }
+}
+int main()
+{
+    Node* newNode= new Node(10);
+
+    Node* head=newNode;
+    Node* tail=newNode;
+
+    insertAtHead(head,12);
+
+    insertAtTail(tail, 20);
+
+    insertAtPosition(head,tail,2,22);
+
+    deleteNode(head, 3);
+
+    print(head);
+}
